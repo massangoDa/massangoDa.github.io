49d15c16aa1f636738a0590cf45a74a036c13a04
massangoDa <122436509+massangoDa@users.noreply.github.com>
2023-02-25 11:59:28 +0900

    Add files via upload

---
+++ ddos/トルネード三式_files/show_ad.php
@@ -0,0 +1,3 @@
+<!--
+document.write("<a href=\"https://ad.broadcaststation.net/ads/url.php?id=WEB11\" target=\"_blank\"><img src=\"https://ad.broadcaststation.net/ads/images/728x90_web11.png\" alt=\"WEB11\" width=\"728\" height=\"90\" border=\"0\" /></a>");
+//-->
